3896cdcb494919fbe04ef39e69d1d340f857eca1
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-07-17 08:33:34 -0700

    upload progress

--- examples/upload.rs
+++ examples/upload.rs
@@ -1,14 +1,24 @@
 use aws_config::BehaviorVersion;
 use aws_sdk_s3::types::StorageClass;
 use rcs3ud::{S3Dest, UploadInput, upload};
+use sipper::Sipper;
+use tokio::fs::File;
 
 #[tokio::main]
 async fn main() {
     let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
     let client = aws_sdk_s3::Client::new(&config);
-    upload(UploadInput {
+    let file = File::options()
+        .read(true)
+        .write(false)
+        .open("README.md")
+        .await
+        .unwrap();
+    let size = file.metadata().await.unwrap().len();
+    println!("Total size: {size}");
+    let mut straw = upload(UploadInput {
         client: &client,
-        src: "README.md",
+        src: file,
         dest: S3Dest {
             bucket: "rcs3ud",
             object_key: "README.md",
@@ -16,5 +26,10 @@ async fn main() {
         },
     })
     .await
-    .unwrap();
+    .pin();
+    while let Some(bytes_read) = straw.sip().await {
+        println!("Read bytes: {bytes_read}");
+    }
+    straw.await.unwrap();
+    println!("Uploaded file");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,17 @@ use std::{io, num::TryFromIntError};
 use aws_sdk_s3::{
     error::SdkError,
     operation::{get_object::GetObjectError, put_object::PutObjectError},
-    primitives::{ByteStream, ByteStreamError},
+    primitives::{ByteStream, ByteStreamError, SdkBody},
     types::StorageClass,
 };
-use sipper::{Straw, sipper};
+use bytes::BytesMut;
+use pin_project_lite::pin_project;
+use sipper::{Straw, Stream, sipper};
 use thiserror::Error;
-use tokio::io::AsyncWriteExt;
+use tokio::{
+    fs::File,
+    io::{AsyncReadExt, AsyncWriteExt},
+};
 
 pub struct S3Dest<'a> {
     pub bucket: &'a str,
@@ -18,35 +23,87 @@ pub struct S3Dest<'a> {
 
 pub struct UploadInput<'a> {
     pub client: &'a aws_sdk_s3::Client,
-    pub src: &'a str,
+    pub src: File,
     pub dest: S3Dest<'a>,
 }
 
 #[allow(clippy::large_enum_variant)]
 #[derive(Debug, Error)]
 pub enum UploadError {
+    #[error("Error getting metadata")]
+    Metadata(io::Error),
+    #[error("This should never happen")]
+    LenConvertError(TryFromIntError),
     #[error("Error with input file")]
     ByteStream(ByteStreamError),
     #[error("Error uploading file")]
     PutObjectError(SdkError<PutObjectError>),
 }
 
-pub async fn upload(input: UploadInput<'_>) -> Result<(), UploadError> {
-    input
-        .client
-        .put_object()
-        .bucket(input.dest.bucket)
-        .key(input.dest.object_key)
-        .storage_class(input.dest.storage_class)
-        .body(
-            ByteStream::from_path(input.src)
-                .await
-                .map_err(UploadError::ByteStream)?,
-        )
-        .send()
-        .await
-        .map_err(UploadError::PutObjectError)?;
-    Ok(())
+pub async fn upload(input: UploadInput<'_>) -> impl Straw<(), usize, UploadError> {
+    sipper(async move |sender| {
+        let src_len = input
+            .src
+            .metadata()
+            .await
+            .map_err(UploadError::Metadata)?
+            .len();
+        input
+            .client
+            .put_object()
+            .bucket(input.dest.bucket)
+            .key(input.dest.object_key)
+            .storage_class(input.dest.storage_class)
+            .content_length(src_len.try_into().map_err(UploadError::LenConvertError)?)
+            .body(ByteStream::from_body_1_x(SdkBody::from_body_1_x(
+                reqwest::Body::wrap_stream({
+                    pin_project! {
+                        struct StreamWithSizeHint<T, S: Stream<Item = T>> {
+                            #[pin]
+                            stream: S,
+                            size_hint: (usize, Option<usize>),
+                        }
+                    }
+                    impl<T, S: Stream<Item = T> + Unpin> Stream for StreamWithSizeHint<T, S> {
+                        type Item = T;
+                        fn poll_next(
+                            self: std::pin::Pin<&mut Self>,
+                            cx: &mut std::task::Context<'_>,
+                        ) -> std::task::Poll<Option<Self::Item>> {
+                            self.project().stream.poll_next(cx)
+                        }
+                        fn size_hint(&self) -> (usize, Option<usize>) {
+                            panic!();
+                            self.size_hint
+                        }
+                    }
+                    StreamWithSizeHint {
+                        stream: Box::pin(futures::stream::try_unfold((input.src, 0, sender), {
+                            async move |(mut src, read_count, mut sender)| {
+                                let mut bytes = BytesMut::new();
+                                let count = src.read_buf(&mut bytes).await?;
+                                if count > 0 {
+                                    let new_read_count = read_count + count;
+                                    sender.send(new_read_count).await;
+                                    Ok::<_, io::Error>(Some((bytes, (src, new_read_count, sender))))
+                                } else {
+                                    Ok(None)
+                                }
+                            }
+                        })),
+                        size_hint: {
+                            let len = src_len.try_into().map_err(UploadError::LenConvertError)?;
+                            (len, Some(len))
+                        },
+                    }
+                }),
+            )))
+            .content_length(src_len.try_into().map_err(UploadError::LenConvertError)?)
+            .send()
+            .await
+            .map_err(UploadError::PutObjectError)?;
+        Ok(())
+    })
 }
 
 pub struct S3Src<'a> {
